3a72e0c0fa835edcd4da9e913ac555e09299d6ac
Mitch Allen <github@mitchallen.com>
2024-03-23 22:10:37 -0400

    feat: added button bar

---
+++ src/app.js
@@ -0,0 +1,5 @@
+
+document.getElementById('btnMusic').addEventListener('click', function () {
+    let iframe = document.getElementById('musicFrame');
+    iframe.style.display = (iframe.style.display === 'none') ? 'block' : 'none';
+});
